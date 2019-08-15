6a20c84220df0c17d02b03f4c1a65c6b085e79fc
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-15 01:43:53 +0000

    Better mixed layer vertical plots

--- plot_vertical_profiles.py
+++ plot_vertical_profiles.py
@@ -16,25 +16,28 @@ matplotlib.rcParams['figure.dpi'] = 200
 plt.style.use('dark_background')
 
 def plot_vertical_profiles(file, field, Is, save=True):
-    rotation_period = file["parameters/rotation_period"][()]
+    rotation_period = 86400
 
     fig, ax1 = plt.subplots(nrows=1, ncols=1, figsize=(9, 16))
     for i in Is:
         i = str(i)
-        t = file["timeseries/t/" + i]
+        t = file["timeseries/t/" + i][()]
         
         Nz, Lz = file["grid/Nz"][()], file["grid/Lz"][()]
         z = linspace(0, -Lz, Nz)
         
         Fp = file["timeseries/" + field + "/" + i][()]
         Fp = reshape(Fp, Nz)
-        
-        ax1.plot(Fp, z / 1000, label="{:.0f} days".format(t / rotation_period))
+
+        if field in ["nu", "kappaT"]:
+            ax1.semilogx(Fp, z, label="{:.2f} days".format(t / rotation_period))
+        else:
+            ax1.plot(Fp, z, label="{:.2f} days".format(t / rotation_period))
     
     ax1.set_title("Horizontally averaged {:s} profiles".format(field))
-    ax1.set_ylim([-100, 0])
+    ax1.set_ylim([-Lz, 0])
     ax1.set_xlabel(f"{field}")
-    ax1.set_ylabel("z (km)")
+    ax1.set_ylabel("z (m)")
     ax1.legend(frameon=False)
 
     if save:
@@ -53,10 +56,11 @@ if __name__ == "__main__":
     Is = sorted(list(map(int, list(file["timeseries/t"].keys()))))  # List of iterations with output.
     logging.info(f"{len(Is):d} vertical profiles found: i={min(Is)}->{max(Is)}")
 
-    Ip = [Is[n] for n in [0, 10, 30, 50, 80]]  # Iterations to plot.
+    Ip = [Is[n] for n in [1, 200, 700, -1]]  # Iterations to plot.
     plot_vertical_profiles(file, "T", Ip)
     plot_vertical_profiles(file, "u", Ip)
     plot_vertical_profiles(file, "v", Ip)
     plot_vertical_profiles(file, "wT", Ip)
-
+    plot_vertical_profiles(file, "nu", Ip)
+    plot_vertical_profiles(file, "kappaT", Ip)
 
