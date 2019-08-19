0a787bc5ec589a7c39bf0864924d26e43df8883d
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-08-19 16:29:17 -0400

    Plot profiles as well

--- plot_oceananigans_output_contourf3d.py
+++ plot_oceananigans_output_contourf3d.py
@@ -19,7 +19,7 @@ plt.switch_backend("Agg")
 
 from joblib import Parallel, delayed
 
-def plot_contourf3d_from_jld2(jld2_filepath, png_filepath, field, i, vmin, vmax, n_contours, cmap="inferno", var_offset=0):
+def plot_contourf3d_from_jld2(slice_filepath, profile_filepath, png_filepath, field, i, vmin, vmax, n_contours, cmap="inferno"):
     # For some reason I need to do the import here so it shows up on all joblib workers.
     from mpl_toolkits.mplot3d import Axes3D
 
@@ -28,19 +28,20 @@ def plot_contourf3d_from_jld2(jld2_filepath, png_filepath, field, i, vmin, vmax,
 
     contour_spacing = (vmax - vmin) / n_contours
 
-    file = h5py.File(jld2_filepath, "r")
+    sfile = h5py.File(slice_filepath, "r")
+    pfile = h5py.File(profile_filepath, "r")
 
     i = str(i)
-    t = file["timeseries/t/" + i][()]
-    Nx, Lx = file["grid/Nx"][()], file["grid/Lx"][()]
-    Ny, Ly = file["grid/Ny"][()], file["grid/Ly"][()]
-    Nz, Lz = file["grid/Nz"][()], file["grid/Lz"][()]
+    t = sfile["timeseries/t/" + i][()]
+    Nx, Lx = sfile["grid/Nx"][()], sfile["grid/Lx"][()]
+    Ny, Ly = sfile["grid/Ny"][()], sfile["grid/Ly"][()]
+    Nz, Lz = sfile["grid/Nz"][()], sfile["grid/Lz"][()]
 
     x, y, z = linspace(0, Lx, Nx), linspace(0, Ly, Ny), linspace(0, Lz, Nz)
    
-    xy_slice = file["timeseries/" + field + "_xy_slice/" + i][()][1:Nx+1, 1:Ny+1]
-    xz_slice = file["timeseries/" + field + "_xz_slice/" + i][()][1:Nx+1, 1:Nz+1]
-    yz_slice = file["timeseries/" + field + "_yz_slice/" + i][()][1:Ny+1, 1:Nz+1]
+    xy_slice = sfile["timeseries/" + field + "_xy_slice/" + i][()][1:Nx+1, 1:Ny+1]
+    xz_slice = sfile["timeseries/" + field + "_xz_slice/" + i][()][1:Nx+1, 1:Nz+1]
+    yz_slice = sfile["timeseries/" + field + "_yz_slice/" + i][()][1:Ny+1, 1:Nz+1]
 
     fig = plt.figure(figsize=(16, 9))
 
@@ -78,12 +79,11 @@ def plot_contourf3d_from_jld2(jld2_filepath, png_filepath, field, i, vmin, vmax,
     ax.set_yticks(linspace(0, Ly, num=5))
     ax.set_zticks(linspace(0, -Lz, num=5))
 
-    '''
-    T_profile = nc_output["T"].mean(dim=["xC", "yC"])
+    T = pfile["timeseries/T/" + i][()]
 
     ax2 = plt.subplot2grid((3, 4), (0, 3))
-    ax2.plot(T_profile.sel(zC=slice(0, -25)) - 273.15, zC.sel(zC=slice(0, -25)))
-    ax2.set_title(r"$\overline{T}(z)$ [°C]")
+    ax2.plot(T, z)
+    ax2.set_title(r"$\overline{T}(z)$ (°C)")
     ax2.set_ylabel("z (m)")
     ax2.set_xlim(19.75, 20)
     ax2.set_ylim(-25, 0)
@@ -91,28 +91,31 @@ def plot_contourf3d_from_jld2(jld2_filepath, png_filepath, field, i, vmin, vmax,
     u, v, w = nc_output["u"], nc_output["v"], nc_output["w"]
     HTKE_profile = 0.5 * mean(square(u.values) + square(v.values), axis=(1, 2))
     VTKE_profile = 0.5 * square(w).mean(dim=["xC", "yC"])
+    
+    uu = pfile["timeseries/uu/" + i][()]
+    vv = pfile["timeseries/vv/" + i][()]
+    ww = pfile["timeseries/ww/" + i][()]
 
     ax3 = plt.subplot2grid((3, 4), (1, 3))
-    ax3.plot(HTKE_profile[0:Nz//4], zC.sel(zC=slice(0, -25)).values, color="tab:orange", label=r"$(u^2 + v^2)/2$")
-    ax3.plot(10 * VTKE_profile.sel(zF=slice(0, -25)), zC.sel(zC=slice(0, -25)), color="tab:green", label=r"$10 \times w^2/2$")
-    ax3.set_title("Turbulent kinetic energy [m$^2$/s$^2$]")
+    ax3.plot(uu + vv, z, color="tab:orange", label=r"$u^2 + v^2$")
+    ax3.plot(10 * ww, z, color="tab:green", label=r"$10 \times w^2$")
+    ax3.set_title("Kinetic energy (m$^2$/s$^2$)")
     ax3.set_ylabel("z (m)")
     ax3.set_xlim(-0.001, 0.02)
     ax3.set_ylim(-25, 0)
-    ax3.legend(loc="lower right")
+    ax3.legend(loc="lower right", frameon=False)
 
     alpha = 2.07e-4
     g = 9.80665
-    T = nc_output["T"]
+    w = pfile["timeseries/wT/" + i][()]
     buoyancy_flux_profile = mean(alpha * g * w.values * T.values, axis=(1, 2))
 
     ax4 = plt.subplot2grid((3, 4), (2, 3))
-    ax4.plot(buoyancy_flux_profile[0:Nz//4], zC.sel(zC=slice(0, -25)).values, color="tab:red")
+    ax4.plot(alpha * g * wT, z, color="tab:red")
     ax4.set_title(r"Buoyancy flux $\alpha g \overline{w' T'}$")
     ax4.set_ylabel("z (m)")
     ax4.set_xlim(-5e-8, 5e-8)
     ax4.set_ylim(-25, 0)
-    '''
 
     # plt.show()
 
@@ -124,9 +127,10 @@ def plot_contourf3d_from_jld2(jld2_filepath, png_filepath, field, i, vmin, vmax,
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Make horizontal slice movies from Oceananigans.jl JLD2 output.")
     parser.add_argument("-s", "--slices", type=str, nargs='+', required=True, help="JLD2 slice output filepaths")
+    parser.add_argument("-p", "--profiles", type=str, required=True, help="JLD2 profile output filepath")
     args = parser.parse_args()
-    filepaths = args.slices
-    sort_nicely(filepaths)
+    slice_filepaths, profile_filepath = args.slices, args.profiles
+    sort_nicely(slice_filepaths)
 
     Is = []  # We'll generate a list of iterations with output across all files.
     for fp in filepaths:
@@ -138,5 +142,6 @@ if __name__ == "__main__":
     logging.info(f"Found {len(Is):d} snapshots per field across {len(filepaths):d} files: i={Is[0][0]}->{Is[-1][0]}")
     
     # Plot many frames in parallel.
-    plot_contourf3d_from_jld2(jld2_filepath=Is[-1][1], i=Is[-1][0], png_filepath="test_frame.png", field="T", vmin=19, vmax=20.05, n_contours=100)
+    plot_contourf3d_from_jld2(slice_filepath=Is[-1][1], profile_filepath=profile_filepath, i=Is[-1][0], png_filepath="test_frame.png",
+                              field="T", vmin=19, vmax=20.05, n_contours=100)
 
