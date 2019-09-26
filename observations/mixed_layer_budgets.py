ec1b373f761c2fc2e6a4aa239d2fe573b146a38a
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-09-26 10:58:34 -0400

    Make plots of 5-year bSOSE averages

--- observations/mixed_layer_budgets.py
+++ observations/mixed_layer_budgets.py
@@ -1,4 +1,5 @@
 import xarray as xr
+from dask.diagnostics import ProgressBar
 
 import cmocean
 import matplotlib.pyplot as plt
@@ -25,4 +26,15 @@ files_2d = [
 
 fields_3d = xr.open_mfdataset(files_3d, parallel=True, chunks={'time': 10})
 fields_2d = xr.open_mfdataset(files_2d, parallel=True, chunks={'time': 10})
+
+vars = ["BLGMLD", "SFLUX", "TFLUX", "oceFWflx", "oceQnet", "oceQsw", "oceTAUX", "oceTAUY"]
+depth = fields_2d.oceQsw.Depth
+for var in vars:
+    print(f"Plotting {var}...")
+    var_avg = fields_2d[var].mean(dim='time')
+    var_avg.attrs = fields_2d[var].attrs
+    with ProgressBar():
+        var_avg.where(depth > 0).plot(robust=True)
+    plt.savefig(f"bSOSE_{var}_average.png", dpi=300)
+    plt.close("all")
 
