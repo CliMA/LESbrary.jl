d4337b70f5e18e0d3105c8f5c1aa072989f48f82
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2019-10-02 11:43:00 -0400

    Update plot_oceananigans_output_contourf3d.py

--- plotting/plot_oceananigans_output_contourf3d.py
+++ plotting/plot_oceananigans_output_contourf3d.py
@@ -8,12 +8,12 @@ import argparse
 
 import h5py
 import ffmpeg
+import cmocean
 import numpy as np
 import matplotlib.pyplot as plt
 
 from numpy import ones, meshgrid, linspace, square, mean
 from mpl_toolkits.mplot3d import axes3d
-from matplotlib.colors import PowerNorm
 
 from human_sorting import sort_nicely
 
@@ -38,10 +38,18 @@ def plot_contourf3d_from_jld2(slice_filepath, profile_filepath, png_filepath, fi
     Nz, Lz = sfile["grid/Nz"][()], sfile["grid/Lz"][()]
 
     x, y, z = linspace(0, Lx, Nx), linspace(0, Ly, Ny), linspace(0, -Lz, Nz)
-   
+
     xy_slice = sfile["timeseries/" + field + "_xy_slice/" + i][()][1:Nx+1, 1:Ny+1]
-    xz_slice = sfile["timeseries/" + field + "_xz_slice/" + i][()][1:Nx+1, 1:Nz+1]
-    yz_slice = sfile["timeseries/" + field + "_yz_slice/" + i][()][1:Ny+1, 1:Nz+1]
+    xz_slice = np.fliplr(sfile["timeseries/" + field + "_xz_slice/" + i][()][1:Nx+1, 1:Nz+1])
+    yz_slice = np.fliplr(sfile["timeseries/" + field + "_yz_slice/" + i][()][1:Ny+1, 1:Nz+1])
+
+    T = pfile["timeseries/T/" + i][()][1:Nz+1, 0, 0]
+
+    if field == "T":
+        T_avg = T[:, np.newaxis]
+        xy_slice = xy_slice - T_avg[0]
+        xz_slice = xz_slice - T_avg
+        yz_slice = yz_slice - T_avg
 
     fig = plt.figure(figsize=(16, 9))
 
@@ -54,15 +62,15 @@ def plot_contourf3d_from_jld2(slice_filepath, profile_filepath, png_filepath, fi
 
     x_offset, y_offset, z_offset = Lx/1000, 0, 0
 
-    vmax = xy_slice.max() + 0.01
     contour_spacing = (vmax - vmin) / n_contours
+    levels = np.arange(vmin, vmax, contour_spacing)
 
-    cf1 = ax.contourf(XC_z, YC_z, xy_slice, zdir="z", offset=z_offset, levels=np.arange(vmin, vmax, contour_spacing), cmap=cmap, norm=PowerNorm(gamma=5))
-    cf2 = ax.contourf(yz_slice, YC_x, ZC_x, zdir="x", offset=x_offset, levels=np.arange(vmin, vmax, contour_spacing), cmap=cmap, norm=PowerNorm(gamma=5))
-    cf3 = ax.contourf(XC_y, xz_slice, ZC_y, zdir="y", offset=y_offset, levels=np.arange(vmin, vmax, contour_spacing), cmap=cmap, norm=PowerNorm(gamma=5))
+    cf1 = ax.contourf(XC_z, YC_z, xy_slice, zdir="z", offset=z_offset, levels=levels, cmap=cmap)
+    cf2 = ax.contourf(yz_slice, YC_x, ZC_x, zdir="x", offset=x_offset, levels=levels, cmap=cmap)
+    cf3 = ax.contourf(XC_y, xz_slice, ZC_y, zdir="y", offset=y_offset, levels=levels, cmap=cmap)
 
-    clb = fig.colorbar(cf3, ticks=[19.0, 19.2, 19.4, 19.6, 19.8, 20.0], shrink=0.9)
-    clb.ax.set_title(r"T (°C)")
+    clb = fig.colorbar(cf1, ticks=[-0.1, -0.05, 0, 0.05, 0.1], shrink=0.9)
+    clb.ax.set_title(r"$T^\prime$ (°C)")
 
     ax.set_xlim3d(0, Lx)
     ax.set_ylim3d(0, Ly)
@@ -74,14 +82,12 @@ def plot_contourf3d_from_jld2(slice_filepath, profile_filepath, png_filepath, fi
 
     ax.view_init(elev=30, azim=-135)
 
-    ax.set_title("t = {:05d} s ({:02.2f} hours)".format(int(t), t / 3600), y=1.05)
+    ax.set_title("Convecting wind stress: temperature anomaly @ t = {:05d} s ({:02.2f} hours)".format(int(t), t / 3600), y=1.05)
 
     ax.set_xticks(linspace(0, Lx, num=5))
     ax.set_yticks(linspace(0, Ly, num=5))
     ax.set_zticks(linspace(0, -Lz, num=5))
 
-    T = pfile["timeseries/T/" + i][()][:, 0, 0]
-    
     ax2 = plt.subplot2grid((3, 4), (0, 3))
     ax2.plot(T, z)
     ax2.set_title(r"$\overline{T}(z)$ (°C)")
@@ -89,28 +95,26 @@ def plot_contourf3d_from_jld2(slice_filepath, profile_filepath, png_filepath, fi
     ax2.set_xlim(19.75, 20)
     ax2.set_ylim(-25, 0)
 
-    uu = pfile["timeseries/uu/" + i][()][:, 0, 0]
-    vv = pfile["timeseries/vv/" + i][()][:, 0, 0]
-    ww = pfile["timeseries/ww/" + i][()][:, 0, 0]
+    uu = np.square(pfile["timeseries/u/" + i][()][1:Nz+1, 0, 0])
+    vv = np.square(pfile["timeseries/v/" + i][()][1:Nz+1, 0, 0])
 
     ax3 = plt.subplot2grid((3, 4), (1, 3))
     ax3.plot(uu + vv, z, color="tab:orange", label=r"$u^2 + v^2$")
-    ax3.plot(10 * ww, z, color="tab:green", label=r"$10 \times w^2$")
     ax3.set_title("Kinetic energy (m$^2$/s$^2$)")
     ax3.set_ylabel("z (m)")
-    ax3.set_xlim(-0.001, 0.02)
+    ax3.set_xlim(-0.001, 0.05)
     ax3.set_ylim(-25, 0)
     ax3.legend(loc="lower right", frameon=False)
 
     alpha = 2.07e-4
     g = 9.80665
-    wT = pfile["timeseries/wT/" + i][()][:, 0, 0]
+    wT = pfile["timeseries/wT/" + i][()][1:Nz+1, 0, 0]
 
     ax4 = plt.subplot2grid((3, 4), (2, 3))
     ax4.plot(alpha * g * wT, z, color="tab:red")
-    ax4.set_title(r"Buoyancy flux $\alpha g \overline{w' T'}$")
+    ax4.set_title(r"Buoyancy flux $\alpha g \overline{w' T'}$ (m$^2$/s$^3$)")
     ax4.set_ylabel("z (m)")
-    ax4.set_xlim(-5e-8, 5e-8)
+    ax4.set_xlim(-10e-8, 10e-8)
     ax4.set_ylim(-25, 0)
 
     # plt.show()
@@ -136,12 +140,16 @@ if __name__ == "__main__":
         Is.extend(tuplified_Is)
 
     logging.info(f"Found {len(Is):d} snapshots per field across {len(slice_filepaths):d} files: i={Is[0][0]}->{Is[-1][0]}")
-    
+
     # Plot many frames in parallel.
     joblib.Parallel(n_jobs=-1)(
-         joblib.delayed(plot_contourf3d_from_jld2)(slice_filepath=I[1], profile_filepath=profile_filepath, i=I[0], png_filepath="convecting_wind_stress_{:05d}.png".format(frame_number),
-                                                   field="T", vmin=19, vmax=20.05, n_contours=100)
-         for frame_number, I in enumerate(Is[1:500]))
+        joblib.delayed(plot_contourf3d_from_jld2)(slice_filepath=I[1], profile_filepath=profile_filepath, i=I[0], png_filepath="convecting_wind_stress_{:05d}.png".format(frame_number),
+                                                  field="T", vmin=-0.105, vmax=0.105, n_contours=100, cmap=cmocean.cm.balance)
+                                                  for frame_number, I in enumerate(Is[1:-1]))
+
+    # for frame_number, I in enumerate(Is[0:15000:1500]):
+    #     plot_contourf3d_from_jld2(slice_filepath=I[1], profile_filepath=profile_filepath, i=I[0], png_filepath="convecting_wind_stress_{:05d}.png".format(frame_number),
+    #                               field="T", vmin=-0.082, vmax=0.082, n_contours=100, cmap=cmocean.cm.balance)
 
     (
         ffmpeg
