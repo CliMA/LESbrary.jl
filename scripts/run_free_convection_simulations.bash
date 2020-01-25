f04cdc6fa15a93591696ae4fe14f847a0d449e53
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-01-25 08:31:20 -0800

    Remove bash run scripts

--- scripts/run_free_convection_simulations.bash
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for dTdz in 0.01 0.05 0.005; do
-    for Q in -10 -50 -100; do
-        julia --project deepening_mixed_layer.jl -N 256 -V 256 -L 100 -H 100 --dTdz $dTdz -Q $Q --wind-stress 0 --days 8 -d ~/data/
-    done
-done
-
