3595ff2e1db6d5e6898b6ea84335fdb9dbd23b15
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2020-03-05 14:37:48 -0500

    Plotting statistics

--- src/make_lesbrary_plots.py
+++ src/make_lesbrary_plots.py
@@ -96,8 +96,64 @@ def plot_large_scale(ds):
 
         plt.close("all")
 
+def plot_statistics(ds):
+    for n in range(0, ds.time.size):
+        fig, axes = plt.subplots(nrows=2, ncols=4, sharey=True, figsize=(16, 9), dpi=300)
+
+        t = ds.time.values[n] / 86400
+        lat, lon = ds.attrs["lat"], ds.attrs["lon"]
+        fig.suptitle(f"Statistics at {lat}°N, {lon}°E, t = {t:.3f} days", fontsize=16)
+
+        u = ds.u.isel(time=n).squeeze()
+        v = ds.v.isel(time=n).squeeze()
+        u.plot(ax=axes[0, 0], y="zC", label="u", color="tab:green")
+        v.plot(ax=axes[0, 0], y="zC", label="v", color="tab:red")
+        axes[0, 0].legend(loc="best", frameon=False)
+        axes[0, 0].set_xlabel("velocity [m/s]")
+
+        T = ds.T.isel(time=n).squeeze()
+        T.plot(ax=axes[0, 1], y="zC")
+
+        S = ds.S.isel(time=n).squeeze()
+        S.plot(ax=axes[0, 2], y="zC")
+
+        uu = ds.uu.isel(time=n).squeeze()
+        vv = ds.vv.isel(time=n).squeeze()
+        ww = ds.ww.isel(time=n).squeeze()
+        uu.plot(ax=axes[1, 0], y="zC", label="$\overline{u^\prime u^\prime}$")
+        vv.plot(ax=axes[1, 0], y="zC", label="$\overline{v^\prime v^\prime}$")
+        ww.plot(ax=axes[1, 0], y="zC", label="$\overline{w^\prime w^\prime}$")
+        axes[1, 0].legend(loc="best", frameon=False)
+        axes[1, 0].set_xlabel("turbulent kinetic energy [m²/s²]")
+
+        uv = ds.uv.isel(time=n).squeeze()
+        uw = ds.uw.isel(time=n).squeeze()
+        vw = ds.vw.isel(time=n).squeeze()
+        uv.plot(ax=axes[1, 1], y="zC", label="$\overline{u^\prime v^\prime}$")
+        uw.plot(ax=axes[1, 1], y="zC", label="$\overline{u^\prime w^\prime}$")
+        vw.plot(ax=axes[1, 1], y="zC", label="$\overline{v^\prime w^\prime}$")
+        axes[1, 1].legend(loc="best", frameon=False)
+        axes[1, 1].set_xlabel("velocity covariance [m²/s²]")
+
+        wT = ds.wT.isel(time=n).squeeze()
+        wT.plot(ax=axes[1, 2], y="zC")
+        axes[1, 2].set_xlabel("$\overline{w^\prime T^\prime}$")
+
+        wS = ds.wS.isel(time=n).squeeze()
+        wS.plot(ax=axes[1, 3], y="zC")
+        axes[1, 3].set_xlabel("$\overline{w^\prime S^\prime}$")
+
+        for ax in axes.reshape(-1):
+            ax.set_title("")
+
+        png_filename = f"statistics_{n:05d}.png"
+        logging.info(f"Saving: {png_filename}...")
+        plt.savefig(png_filename)
+
+        plt.close("all")
+
 def plot_slices(ds):
-    for n in range(0, ds.time.size, 1):
+    for n in range(0, ds.time.size):
         fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 9), dpi=300)
 
         t = ds.time.values[n] / 86400
@@ -126,12 +182,8 @@ def plot_slices(ds):
 
         plt.close("all")
 
-# def plot_profiles(ds):
-#     for n in range(0, ds.time.size, 1):
-#         fig, axes = plt.subplots(nrows)
-
 def plot_surfaces(ds):
-    for n in range(0, ds.time.size, 1):
+    for n in range(0, ds.time.size):
         fig, axes = plt.subplots(nrows=2, ncols=2, figsize=(16, 9), dpi=300)
 
         t = ds.time.values[n] / 86400
@@ -168,10 +220,16 @@ def make_lesbrary_plots(lat, lon, days):
     dsl = xr.open_dataset(f"lesbrary_lat{lat}_lon{lon}_days{days}_large_scale.nc")
 
     plot_forcing(dsl)
+
     plot_large_scale(dsl)
     make_movie("large_scale_%05d.png", "large_scale.mp4")
+
+    plot_statistics(dsp)
+    make_movie("statistics_%05d.png", "statistics.mp4")
+
     plot_slices(dsx)
     make_movie("slice_%05d.png", "slice.mp4")
+
     plot_surfaces(dss)
     make_movie("surface_%05d.png", "surface.mp4")
 
