3de15b65aed1b81a8a0ab1a00b3bd466204d6ac8
ali-ramadhan <ali.hh.ramadhan@gmail.com>
2021-05-21 11:09:12 -0400

    Fix `diagnose_buoyancy_flux` docstring

--- src/Utils/diagnose_buoyancy_flux.jl
+++ src/Utils/diagnose_buoyancy_flux.jl
@@ -4,16 +4,15 @@ using QuadGK
     diagnose_buoyancy_flux(b, z, time_period, depth)
 
 Returns the buoyancy flux (m²/s³) required to extract all the buoyancy from a 1D buoyancy profile `b(z)`
-down to some z-coordinate `depth` (< 0) over a `time_period` (s) by integrating the buoyancy profile and
-converting the integrated buoyancy content to an equivalent buoyancy flux:
+down to some z-coordinate `depth` (< 0) over a `time_period` τ (s) by integrating the buoyancy profile
+and converting the integrated buoyancy content to an equivalent buoyancy flux:
 
-          ⌠ z_surface
-    Qb =  |           [b(z) - b(depth)] dz
-          ⌡ depth
+          1  ⌠ z_surface
+    Qb = --- |           [b(z) - b(depth)] dz
+          τ  ⌡ depth
 """
 function diagnose_buoyancy_flux(b, z, time_period, depth)
 
-    Δτ = time_period
     Δz = z[2] - z[1]
     z_surface = last(z)
     @assert first(z) < depth < z_surface
@@ -24,5 +23,5 @@ function diagnose_buoyancy_flux(b, z, time_period, depth)
     integrand(z) = ℑb(z) - b₀
     B, err = quadgk(integrand, depth, z_surface)
 
-    return B / Δτ
+    return B / time_period
 end
