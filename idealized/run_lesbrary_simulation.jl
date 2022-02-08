2b89d6bdf73e26f513d9b9e51953b158ed1bda94
Gregory Wagner <wagner.greg@gmail.com>
2022-02-08 14:34:28 -0500

    Updates

--- idealized/run_lesbrary_simulation.jl
+++ idealized/run_lesbrary_simulation.jl
@@ -8,16 +8,38 @@ using LESbrary.IdealizedExperiments: two_day_suite_parameters
 using LESbrary.IdealizedExperiments: four_day_suite_parameters
 using LESbrary.IdealizedExperiments: six_day_suite_parameters
 
-# 
-# Run 
-# case = :free_convection
+# LESbrary parameters
+# ===================
 #
+# Canonical LESbrary parameters are organized by the _intended duration_ of the simulation
+# --- two days for the "two day suite", four days for the "four day suite", et cetera.
+# The strength of the forcing is tuned roughly so that the boundary layer deepens to roughly
+# 128 meters, half the depth of a 256 meter domain.
+#
+# Each suite has five cases:
+#
+# * :free_convection
+# * :weak_wind_strong_cooling
+# * :strong_wind_weak_cooling
+# * :strong_wind
+# * :strong_wind_no_rotation
+#
+# In addition to selecting the architecture, size, and case to run, we can also tweak
+# certain parameters. Below we change the "snapshot_time_interval" (the interval over
+# which slices of the simulation is saved) from the default 10 minutes to 1 minute
+# (to make pretty movies), and we turn passive tracers off.
+
 architecture = GPU()
 size = (256, 256, 256)
-case = :weak_wind_strong_cooling
+case = :strong_wind
 snapshot_time_interval = 1minutes
+passive_tracers = false
 
-parameters = two_day_suite_parameters[case]
-simulation = three_layer_constant_fluxes_simulation(; architecture, size, snapshot_time_interval, parameters...)
+simulation = three_layer_constant_fluxes_simulation(; architecture,
+                                                      size,
+                                                      passive_tracers,
+                                                      snapshot_time_interval,
+                                                      two_day_suite_parameters[case]...)
+
 run!(simulation)
-    
+
