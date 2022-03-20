ac389335a6a9ab92b98b1ecb49ddf129e8884c3f
sandreza <andrenogueirasouza@gmail.com>
2022-03-20 13:15:44 -0400

    udpate quickviz for topslice check

--- examples/quick_viz.jl
+++ examples/quick_viz.jl
@@ -1,4 +1,5 @@
-using JLD2, GLMakie, Statistics, Random
+using JLD2, Statistics, Random
+using GLMakie
 
 jl_file = jldopen("/storage4/andre/lesbrary_mesoscale/eddying_channel_zonal_time_average.jld2")
 
@@ -13,3 +14,14 @@ println("clims are ", clims)
 
 heatmap1 = heatmap!(ax, 0 .. 1e6, -3000 .. 0, field, interpolate=true, colormap=:balance, colorrange=clims)
 contour!(ax, 0 .. 1e6, -3000 .. 0, b_field, levels=20, linewidth=4, color=:black, alpha=0.5)
+
+##
+jlfts = jldopen("/storage4/andre/lesbrary_mesoscale/eddying_channel_top_slice.jld2") #jld2 file top slice
+
+t_keys = keys(jlfts["timeseries"]["b"])
+top_bfield = jlfts["timeseries"]["b"][t_keys[end]][:, :, 1]
+
+##
+fig2 = Figure()
+ax2 = Axis(fig2[1, 1])
+heatmap!(ax2, top_bfield, interpolate=true, colormap=:thermometer)
