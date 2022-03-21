499dabf0f816c4ec8facd5e53ffbf5cdc77ea117
Gregory Wagner <wagner.greg@gmail.com>
2022-03-21 12:25:22 -0400

    Updates eddying channel example

--- examples/run_eddying_channel.jl
+++ examples/run_eddying_channel.jl
@@ -3,9 +3,23 @@ using Oceananigans
 using Oceananigans.Units
 using LESbrary.IdealizedExperiments: eddying_channel_simulation
 
-simulation = eddying_channel_simulation(architecture=GPU(), stop_time=2years)
+#####
+##### Setup and run the simulation
+#####
+
+simulation = eddying_channel_simulation(architecture=GPU(), size=(160, 80, 40), stop_time=1years)
  
+wall_time = time_ns()
+
 run!(simulation)
+
+elapsed = 1e-9 * (time_ns() - wall_time)
+
+@info "Simulation took " * prettytime(elapsed) * " to run."
+
+#####
+##### Visualization
+#####
 
 u, v, w = simulation.model.velocities
 ζ = Field(∂x(v) - ∂y(u))
