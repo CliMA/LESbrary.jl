448563c4e3b4265a9d88218360bd2e6499caad95
Gregory Wagner <wagner.greg@gmail.com>
2022-03-21 10:33:29 -0400

    Updates

--- examples/run_eddying_channel.jl
+++ examples/run_eddying_channel.jl
@@ -1,4 +1,6 @@
 using Oceananigans
 using LESbrary.IdealizedExperiments: eddying_channel_simulation
 
-simulation = eddying_channel_simulation(architecture=GPU(), max_momentum_flux=1e-4, max_buoyancy_flux=1e-8)
+simulation = eddying_channel_simulation(architecture=GPU())
+
+run!(simulation)
