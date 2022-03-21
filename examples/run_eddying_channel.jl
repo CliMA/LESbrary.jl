a328cb51279df2fe2a9508935eb37f0b66054791
Gregory Wagner <wagner.greg@gmail.com>
2022-03-21 12:10:35 -0400

    Add some viz to run_eddying_channel

--- examples/run_eddying_channel.jl
+++ examples/run_eddying_channel.jl
@@ -1,6 +1,25 @@
+using GLMakie
 using Oceananigans
+using Oceananigans.Units
 using LESbrary.IdealizedExperiments: eddying_channel_simulation
 
-simulation = eddying_channel_simulation(architecture=GPU())
-
+simulation = eddying_channel_simulation(architecture=GPU(), stop_time=1year)
+ 
 run!(simulation)
+
+u, v, w = simulation.model.velocities
+ζ = Field(∂x(v) - ∂y(u))
+compute!(ζ)
+
+b = simulation.model.tracers.b
+Nz = simulation.model.grid.Nz
+bn = Array(interior(b, :, :, Nz))
+ζn = Array(interior(ζ, :, :, Nz))
+
+fig = Figure(resolution=(1200, 800))
+ax_b = Axis(fig[1, 1])
+ax_ζ = Axis(fig[1, 2])
+heatmap!(ax_b, bn) 
+heatmap!(ax_ζ, ζn) 
+
+display(fig)
