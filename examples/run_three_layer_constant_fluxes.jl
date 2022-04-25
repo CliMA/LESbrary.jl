0cc008927beb9880d17e7d198efb163c3c0f9be3
Gregory Wagner <wagner.greg@gmail.com>
2022-04-25 18:33:16 -0400

    Update packages and three layer run script

--- examples/run_three_layer_constant_fluxes.jl
+++ examples/run_three_layer_constant_fluxes.jl
@@ -6,6 +6,9 @@ using Oceananigans.Units
 using LESbrary.IdealizedExperiments: three_layer_constant_fluxes_simulation
 using LESbrary.IdealizedExperiments: two_day_suite_parameters
 
+using CairoMakie
+using ElectronDisplay
+
 # LESbrary parameters
 # ===================
 #
@@ -33,15 +36,24 @@ configuration = (;
     time_averaged_statistics = false,
 )
 
+#=
 for parameters in values(two_day_suite_parameters)
     @show "Running with $parameters..."
     simulation = three_layer_constant_fluxes_simulation(; configuration..., parameters...)
     run!(simulation)
 end
+=#
 
-#=
-using CairoMakie
-using ElectronDisplay
+p = (name="free_convection",          momentum_flux = 0.0,     buoyancy_flux = 2.2e-7, f = 1e-4, stop_time = 1day)
+# p = (name="weak_wind_strong_cooling", momentum_flux = -4.0e-4, buoyancy_flux = 2.0e-7, f = 1e-4, stop_time = 1day)
+# p = (name="med_wind_med_cooling",     momentum_flux = -6.0e-4, buoyancy_flux = 1.8e-7, f = 1e-4, stop_time = 1day)
+# p = (name="strong_wind_weak_cooling", momentum_flux = -8.0e-4, buoyancy_flux = 4.0e-8, f = 1e-4, stop_time = 1day)
+# p = (name="strong_wind",              momentum_flux = -1.0e-3, buoyancy_flux = 0.0,    f = 1e-4, stop_time = 1day)
+# p = (name="strong_wind_no_rotation,  momentum_flux = -4.0e-4, buoyancy_flux = 0.0,    f = 0.0,  stop_time = 1day)
+
+@show "Running with $p..."
+#simulation = three_layer_constant_fluxes_simulation(; configuration..., p...)
+#run!(simulation)
 
 filepath = simulation.output_writers[:statistics].filepath
 
@@ -49,28 +61,30 @@ u = FieldTimeSeries(filepath, "u", architecture=CPU())
 v = FieldTimeSeries(filepath, "v", architecture=CPU())
 T = FieldTimeSeries(filepath, "T", architecture=CPU())
 e = FieldTimeSeries(filepath, "e", architecture=CPU())
+Nt = length(e.times)
 
 z = znodes(u)
 
 fig = Figure(resolution=(1800, 600))
-ax_T = Axis(fig[1, 1], xlabel="Temperature (ᵒC)", ylabel="z (m)", title=parameters[:name])
-ax_u = Axis(fig[1, 2], xlabel="Velocity (cm s⁻¹)", ylabel="z (m)", title=parameters[:name])
-ax_e = Axis(fig[1, 3], xlabel="Turbulent kinetic energy (cm² s⁻²)", ylabel="z (m)", title=parameters[:name])
+ax_T = Axis(fig[1, 1], xlabel="Temperature (ᵒC)", ylabel="z (m)", title=p[:name])
+ax_u = Axis(fig[1, 2], xlabel="Velocity (cm s⁻¹)", ylabel="z (m)", title=p[:name])
+ax_e = Axis(fig[1, 3], xlabel="Turbulent kinetic energy (cm² s⁻²)", ylabel="z (m)", title=p[:name])
 
-n = length(u.times)
+slider = Slider(fig[2, 1:3], range=1:Nt, startvalue=1)
+n = slider.value
 
-Tn = interior(T[n])[1, 1, :]
-un = interior(u[n])[1, 1, :]
-vn = interior(v[n])[1, 1, :]
-en = interior(e[n])[1, 1, :]
+Tn = @lift interior(T[$n], 1, 1, :)
+un = @lift interior(u[$n], 1, 1, :)
+vn = @lift interior(v[$n], 1, 1, :)
+en = @lift interior(e[$n], 1, 1, :)
 
-@show Tmin = minimum(Tn)
-@show Tmax = maximum(Tn)
+@show Tmin = minimum(T)
+@show Tmax = maximum(T)
 hlines!(ax_T, -128, xmin=Tmin, xmax=Tmax, color=:gray23)
 lines!(ax_T, Tn, z)
 
-@show umin = min(minimum(1e2 * un), minimum(1e2 * vn))
-@show umax = max(maximum(1e2 * un), maximum(1e2 * vn))
+@show umin = min(minimum(1e2 * u), minimum(1e2 * v))
+@show umax = max(maximum(1e2 * u), maximum(1e2 * v))
 hlines!(ax_u, -128, xmin=umin, xmax=umax, color=:gray23)
 lines!(ax_u, 1e2 * un, z)
 lines!(ax_u, 1e2 * vn, z)
@@ -81,4 +95,4 @@ hlines!(ax_e, -128, xmin=emin, xmax=emax, color=:gray23)
 lines!(ax_e, 1e4 * en, z)
 
 display(fig)
-=#
+
