3a8b7e89488ed5058bbf08bd2de45d58df8c6ac1
Gregory Wagner <wagner.greg@gmail.com>
2024-03-31 18:33:34 -0400

    Fix up run_lesbrary_simulation

--- idealized/run_lesbrary_simulation.jl
+++ idealized/run_lesbrary_simulation.jl
@@ -4,9 +4,9 @@ using Oceananigans
 using Oceananigans.Units
 
 using LESbrary.IdealizedExperiments: three_layer_constant_fluxes_simulation
-using LESbrary.IdealizedExperiments: two_day_suite_parameters
-using LESbrary.IdealizedExperiments: four_day_suite_parameters
-using LESbrary.IdealizedExperiments: six_day_suite_parameters
+using LESbrary.IdealizedExperiments: forty_eight_hour_suite_parameters
+using LESbrary.IdealizedExperiments: twenty_four_hour_suite_parameters
+using LESbrary.IdealizedExperiments: twelve_hour_suite_parameters
 
 # LESbrary parameters
 # ===================
@@ -20,6 +20,7 @@ using LESbrary.IdealizedExperiments: six_day_suite_parameters
 #
 # * :free_convection
 # * :weak_wind_strong_cooling
+# * :med_wind_med_cooling
 # * :strong_wind_weak_cooling
 # * :strong_wind
 # * :strong_wind_no_rotation
@@ -27,19 +28,24 @@ using LESbrary.IdealizedExperiments: six_day_suite_parameters
 # In addition to selecting the architecture, size, and case to run, we can also tweak
 # certain parameters. Below we change the "snapshot_time_interval" (the interval over
 # which slices of the simulation is saved) from the default 2 minutes to 1 minute
-# (to make pretty movies), and we turn passive tracers off.
+# (to make pretty movies).
 
 architecture = GPU()
-size = (256, 256, 256)
-case = :strong_wind
+size = (64, 64, 64)
+# case = :strong_wind
 snapshot_time_interval = 1minute
-passive_tracers = false
+data_directory = "/nobackup/users/glwagner/LESbrary/"
 
-simulation = three_layer_constant_fluxes_simulation(; architecture,
-                                                      size,
-                                                      passive_tracers,
-                                                      snapshot_time_interval,
-                                                      two_day_suite_parameters[case]...)
+#for case in [:strong_wind]
+#for case in [:free_convection, :weak_wind_strong_cooling]
+#for case in [:med_wind_med_cooling, :strong_wind_weak_cooling]
+for case in [:strong_wind_no_rotation]
+    simulation = three_layer_constant_fluxes_simulation(; architecture,
+                                                          size,
+                                                          data_directory,
+                                                          snapshot_time_interval,
+                                                          twelve_hour_suite_parameters[case]...)
 
-run!(simulation)
+    run!(simulation)
+end
 
